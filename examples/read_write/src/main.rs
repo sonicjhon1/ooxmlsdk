a691fb1109273453f5e57b098933609e28e98d77
sonicj <sonicjhon56@gmail.com>
2025-12-18 10:48:34 +0700

    docs(examples): Merge multiple read and write examples into one

---
+++ examples/read_write/src/main.rs
@@ -0,0 +1,74 @@
+use ooxmlsdk::{
+    common::Deserializeable,
+    parts::{
+        presentation_document::PresentationDocument, spreadsheet_document::SpreadsheetDocument,
+        wordprocessing_document::WordprocessingDocument,
+    },
+    schemas::schemas_openxmlformats_org_spreadsheetml_2006_main::Worksheet,
+};
+use rootcause::prelude::*;
+use std::{fs::File, io::BufReader};
+use tempfile::tempfile;
+
+const SAMPLE_DOCX_FILE_PATH: &str = "examples/read_docx/samples/demo.docx";
+const SAMPLE_PPTX_FILE_PATH: &str = "examples/read_pptx/samples/demo.pptx";
+const SAMPLE_XLSX_FILE_PATH: &str = "examples/read_xlsx/samples/demo.xlsx";
+const SAMPLE_XML_FILE_PATH: &str = "examples/read_write_xml/samples/sheet1.xml";
+
+fn main() -> Result<(), Report> {
+    {
+        let docx = WordprocessingDocument::new_from_file(SAMPLE_DOCX_FILE_PATH)?;
+        let docx_xml = docx.main_document_part.root_element.to_xml()?;
+        println!("{docx_xml}");
+        assert!(docx.main_document_part.root_element.validate()?);
+
+        let reader = BufReader::new(File::open(SAMPLE_DOCX_FILE_PATH)?);
+        let reader_docx = WordprocessingDocument::new(reader)?;
+        assert_eq!(
+            reader_docx.main_document_part.root_element.to_xml()?,
+            docx_xml
+        );
+
+        let temp_docx_file = tempfile()?;
+        docx.save(temp_docx_file)?;
+    }
+
+    {
+        let pptx = PresentationDocument::new_from_file(SAMPLE_PPTX_FILE_PATH)?;
+        let pptx_xml = pptx.presentation_part.root_element.to_xml()?;
+        println!("{pptx_xml}");
+        assert!(pptx.presentation_part.root_element.validate()?);
+
+        let reader = BufReader::new(File::open(SAMPLE_PPTX_FILE_PATH)?);
+        let reader_pptx = PresentationDocument::new(reader)?;
+        assert_eq!(
+            reader_pptx.presentation_part.root_element.to_xml()?,
+            pptx_xml
+        );
+
+        let temp_pptx_file = tempfile()?;
+        pptx.save(temp_pptx_file)?;
+    }
+
+    {
+        let xlsx = SpreadsheetDocument::new_from_file(SAMPLE_XLSX_FILE_PATH).unwrap();
+        let xlsx_xml = xlsx.workbook_part.root_element.to_xml()?;
+        println!("{xlsx_xml}");
+        assert!(xlsx.workbook_part.root_element.validate()?);
+
+        let reader = BufReader::new(File::open(SAMPLE_XLSX_FILE_PATH)?);
+        let reader_xlsx = SpreadsheetDocument::new(reader)?;
+        assert_eq!(reader_xlsx.workbook_part.root_element.to_xml()?, xlsx_xml);
+
+        let temp_xlsx_file = tempfile()?;
+        xlsx.save(temp_xlsx_file)?;
+    }
+
+    {
+        let worksheet = Worksheet::from_file(SAMPLE_XML_FILE_PATH).unwrap();
+        let xml = worksheet.to_xml()?;
+        println!("{xml}");
+    }
+
+    Ok(())
+}
--- examples/read_write_xml/src/main.rs
+++
@@ -1,9 +0,0 @@
-use ooxmlsdk::{
-    common::Deserializeable, schemas::schemas_openxmlformats_org_spreadsheetml_2006_main::Worksheet,
-};
-
-fn main() {
-    let value = Worksheet::from_file("examples/read_write_xml/samples/sheet1.xml").unwrap();
-
-    println!("{value}");
-}
